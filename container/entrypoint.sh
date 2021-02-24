3f4903a12f9d514adc71cbacd91ec46c683c475b
k3rnelpan1c-dev <69395733+k3rnelpan1c-dev@users.noreply.github.com>
2021-02-24 21:37:31 +0100

    fix entrypoint script if

--- container/entrypoint.sh
+++ container/entrypoint.sh
@@ -23,7 +23,7 @@ if [[ "${1}" == 'dockerhub_ratelimit_exporter' ]]; then
   file_env 'DH_PWD'
   file_env 'DH_USR' "${DH_USR:-}"
 
-  if [[ -z ${DH_USR} && -z ${DH_PWD} ]]; then
+  if [[ -n ${DH_USR} && -n ${DH_PWD} ]]; then
     exec dockerhub_ratelimit_exporter -listen "${LISTEN:-127.0.0.1:9767}" -username "${DH_USR}" -password "${DH_PWD}"
   else
     exec dockerhub_ratelimit_exporter -listen "${LISTEN:-127.0.0.1:9767}"
